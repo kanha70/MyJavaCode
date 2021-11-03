139a45d5e3287e0a1cfc1fcd784383492cd954c3
Kanhaiya <kanhappti.kl@gmail.com>
2021-11-03 23:52:16 +0530

    Initial

---
+++ src/main/java/StringOperation.java
@@ -0,0 +1,57 @@
+public class StringOperation {
+    public static void main(String[] args) {
+        char[] ch = {'K','a','n','h','a'};
+       // String kl = new String(ch);
+        String klp = "Kanhaiya you are so smart";
+        String klp3 = new String("Hi Kanha");
+
+              //Creation of String by Different Ways:
+
+       // System.out.println("Char Array to String :"+kl);
+       // System.out.println("Creating String by string Literal :"+klp);
+       // System.out.println("Creating String by Using New Keywords :"+klp3);
+       // System.out.println("Length of String : "+kl.length());
+
+            //Reversing of String
+
+     //   System.out.println("Reverse String of: "+"'"+kl+"'"+" is");
+     //
+     //   for (int i = 0;i<kl.length();i++){
+     //       System.out.print(kl.charAt(kl.length()-i-1));
+     //   }
+
+        //    charAt implementation
+
+       //   char my = klp.charAt(2);
+       // System.out.println(kl);
+       // System.out.println("Index 2 character : "+my);
+
+       //  Accessing First and Last character
+       // System.out.println(klp);
+       // System.out.println("Initial character is: "+"'"+klp.charAt(0)+"'"+ " and "+"Last charecter is :"+ " '"+ klp.charAt(klp.length()-1)+"'");
+
+         //Printing odd Position character only
+      // for (int i=0;i<klp.length();i++){
+      //     if (i%2==0) System.out.println("Char at "+i+ "place is "+klp.charAt(i));
+      // }
+
+        // Find Number of White Space
+      //  int with = klp.length();
+      //
+      //  System.out.println("In the String: "+klp);
+      //  klp = klp.replace(" ","");
+      //  int without = klp.length();
+      //  System.out.println(with-without);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
