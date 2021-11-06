072a8f62b792c1b49fb7275269548fe007ac72a5
Kanhaiya <kanhappti.kl@gmail.com>
2021-11-06 21:14:46 +0530

    Initial

---
+++ src/main/java/Loops.java
@@ -0,0 +1,15 @@
+public class Loops {
+    public static void main(String[] args) {
+
+    //ForEach Loops
+          // String[] sweets = {"Barfi","Laddu","Rasgulla"};
+          // int i=0;
+          // for (String me : sweets) System.out.println(i++ +" "+me);
+
+
+
+
+
+
+    }
+}
